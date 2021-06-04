ff7c095b05ee6cee9aad8bcd323aa66c12046481
Fedrick Siagian <superiorsiagian@gmail.com>
2021-06-04 21:39:21 +0700

    table tugas 6

---
+++ Tugas6_FedrickSulaimanSiagian/file.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Aplikasi Kalkulator Sederhana</h1>
+        <hr>
+    </div>
+    <div>
+        <table>
+            <th>Operasi</th>
+            <th>Hasil</th>
+            <tr>
+                <td>Penjumlahan</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Pengurangan</td>
+            </tr>
+            <tr>
+                <td>Perkalian</td>
+            </tr>
+            <tr>
+                <td>Pembagian</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
